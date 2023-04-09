19babc8d76fcf3a08c2b4bbb4f0e8f1509af832a
Ефимов Максим <efimov_m@itstep.academy>
2023-04-09 17:49:37 +0300

    Добавьте файлы проекта.

---
+++ NetLinqAllAnyApp/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLinqAllAnyApp
+{
+    public class Employee
+    {
+        public string? Name { set; get; }
+        public int Age { set; get; }
+    }
+
+    public class EmployeNameAgeComparer : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee? x, Employee? y)
+        {
+            return x.Name.Equals(y.Name) && x.Age == y.Age;
+        }
+
+        public int GetHashCode([DisallowNull] Employee obj)
+        {
+            return obj.Name.GetHashCode() + obj.Age.GetHashCode();
+        }
+    }
+}
---
+++ NetLinqAllAnyApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using NetLinqAllAnyApp;
+
+List<Employee> employees = new List<Employee>()
+{
+    new() { Name = "Bob", Age = 29 },
+    new() { Name = "Joe", Age = 37 },
+    new() { Name = "Tom", Age = 23 },
+    new() { Name = "Sam", Age = 19 },
+    new() { Name = "Leo", Age = 43 },
+    new() { Name = "Ann", Age = 38 },
+};
+
+var all18 = employees.All(e => e.Age >= 20);
+Console.WriteLine($"{all18}");
+
+var any40 = employees.Any(e => e.Age >= 50);
+Console.WriteLine(any40);
+
+Employee employee = new() { Name = "Bob", Age = 39 };
+var cont = employees.Contains(employee, new EmployeNameAgeComparer());
+Console.WriteLine(cont);
+
+var first = employees.First(e => e.Age > 50);
+if(first != null)
+    Console.WriteLine(first.Name);
